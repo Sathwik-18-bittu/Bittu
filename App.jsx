7ac9a92baf3f4194ed4e134f992c5f42e7bb041e
Sathwik-18-bittu <sathwikkumbum@gmail.com>
2024-09-25 01:01:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Eventdemo from './components/Eventdemo';
+import First from './components/first/First';
+import MainPage from './components/google/MainPage';
+import Products from './components/Products';
+import Second from './components/second/Second';
+import StateChangeDemo from './components/StateChangeDemo';
+
+function App(){
+  return(
+    <div>
+     {/* <Products /> */}
+     {/* <Eventdemo /> */}
+     {/* <StateChangeDemo /> */}
+     <MainPage />
+    </div>
+  )
+}
+export default App;
